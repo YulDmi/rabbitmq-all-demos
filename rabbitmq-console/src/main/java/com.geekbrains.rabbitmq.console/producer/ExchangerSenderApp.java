32c663510352e775a584eb5ec84d23fd3d376500
YulDmi <barsetca@rambler.ru>
2020-11-23 21:33:22 +0300

    lesson6 - без модуля

--- rabbitmq-console/src/main/java/com.geekbrains.rabbitmq.console/producer/ExchangerSenderApp.java
+++ rabbitmq-console/src/main/java/com.geekbrains.rabbitmq.console/producer/ExchangerSenderApp.java
@@ -6,20 +6,23 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeoutException;
 
 public class ExchangerSenderApp  {
-    private final static String EXCHANGE_NAME = "example-2";
+  private final static String EXCHANGE_NAME = "example-2";
 
     public static void main(String[] args) {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try(Connection connection = factory.newConnection();
             Channel channel = connection.createChannel()){
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
-            String massage = "info: Hello world";
-            channel.basicPublish(EXCHANGE_NAME, "java", null, massage.getBytes("UTF-8"));
-            System.out.println("Sent : " + massage);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            byte[] array = Files.readAllBytes(Paths.get("C:\\Users\\ASUS\\IdeaProjects\\Cloud\\rabbitmq-all-demos\\rabbitmq-console\\src\\main\\resources\\text.txt"));
+            channel.basicPublish(EXCHANGE_NAME, "php", null, array);
+            channel.basicPublish(EXCHANGE_NAME, "java", null, array);
+            System.out.println("Sent massage ");
         } catch (TimeoutException | IOException e) {
             e.printStackTrace();
         }
